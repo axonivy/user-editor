ccc255862e0373f2404332ae8e82a75b4ed74207
Lukas Lieb <lukas.lieb@axonivy.com>
2026-01-20 15:13:07 +0100

    Fix error-boundary update, remove base-ui

--- packages/editor/src/editor/main/ErrorFallback.tsx
+++ packages/editor/src/editor/main/ErrorFallback.tsx
@@ -1,9 +1,10 @@
 import { PanelMessage } from '@axonivy/ui-components';
 import { IvyIcons } from '@axonivy/ui-icons';
-import type { FallbackProps } from 'react-error-boundary';
+import { getErrorMessage, type FallbackProps } from 'react-error-boundary';
 import { useTranslation } from 'react-i18next';
 
 export const ErrorFallback = (props: FallbackProps) => {
   const { t } = useTranslation();
-  return <PanelMessage icon={IvyIcons.ErrorXMark} message={t('message.errorOnly', { error: props.error.message })} />;
+  const message = getErrorMessage(props.error);
+  return <PanelMessage icon={IvyIcons.ErrorXMark} message={t('message.errorOnly', { error: message })} />;
 };
