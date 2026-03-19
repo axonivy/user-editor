baced60f95912f860438dc581539e34c945dfe77
Lukas Lieb <lukas.lieb@axonivy.com>
2026-03-19 14:38:47 +0100

    Suppress i18next log

--- packages/editor/src/test-utils/test-utils.tsx
+++ packages/editor/src/test-utils/test-utils.tsx
@@ -33,6 +33,7 @@ type ContextHelperProps = {
 const initTranslation = () => {
   if (i18n.isInitializing || i18n.isInitialized) return;
   i18n.use(initReactI18next).init({
+    showSupportNotice: false,
     supportedLngs: ['en'],
     fallbackLng: 'en',
     ns: ['user-editor'],
